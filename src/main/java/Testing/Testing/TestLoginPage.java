154666a449ed6e2722b7bc98fdb69dd41f4189a2
Yash Patel <91671051+ydpatel0309@users.noreply.github.com>
2024-02-24 19:23:14 +0530

    Implemented JUnit to test the testcases

--- src/main/java/Testing/Testing/TestLoginPage.java
+++ src/main/java/Testing/Testing/TestLoginPage.java
@@ -1,63 +1,80 @@
 package Testing.Testing;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestLoginPage {
 
-    public static void main(String[] args) throws InterruptedException {
+    private WebDriver driver;
+
+    @Before 
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "D:\\seleniumjd\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/login");
+        driver.manage().window().maximize();
+    }
 
-        // Test Case 1: Correct Username and Correct Password
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    // Test Case 1
+    public void testCorrectCredentials() throws InterruptedException {
         testCaseCorrectCredentials("YashPatel", "Ydpatel@393");
+    }
 
-        // Test Case 2: Wrong Username and Correct Password
+    @Test
+    // Test Case 2
+    public void testWrongUsername() throws InterruptedException {
         testCaseWrongUsername("Ydpatel", "Ydpatel@393");
+    }
 
-        // Test Case 3: Correct Username and Wrong Password
+    @Test
+    // Test Case 3
+    public void testWrongPassword() throws InterruptedException {
         testCaseWrongPassword("YashPatel", "YashPatel");
-        
-        //Test Case 4: Both wrong
-        
-        testCaseWrongBoth("Yash","Patel");
+    }
+
+    @Test
+    // Test Case 4
+    public void testWrongBoth() throws InterruptedException {
+        testCaseWrongBoth("Yash", "Patel");
     }
 
     // Test Case 1: True Positive
-    public static void testCaseCorrectCredentials(String username, String password) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/login");
-        driver.manage().window().maximize();
-
+    public void testCaseCorrectCredentials(String username, String password) throws InterruptedException {
         driver.findElement(By.id("userName")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login")).click();
 
         Thread.sleep(3000);
-		
-		driver.findElement(By.id("searchBox")).sendKeys("Git");
-		
-		Thread.sleep(3000);
-		
-		driver.findElement(By.id("gotoStore")).click();
-		
-		Thread.sleep(3000);
-		
+
+        driver.findElement(By.id("searchBox")).sendKeys("Git");
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("gotoStore")).click();
+
+        Thread.sleep(3000);
+
         if (driver.getCurrentUrl().equals("https://demoqa.com/login")) {
             System.out.println("Test Case - Correct Username and Correct Password: Login unsuccessful.");
         } else {
             System.out.println("Test Case - Correct Username and Correct Password: Login successful!");
         }
-        
-        driver.quit();
     }
 
-    // Test Case 2:  False Positive
-    public static void testCaseWrongUsername(String username, String password) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/login");
-        driver.manage().window().maximize();
-
+    // Test Case 2: False Positive
+    public void testCaseWrongUsername(String username, String password) throws InterruptedException {
         driver.findElement(By.id("userName")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login")).click();
@@ -69,16 +86,10 @@ public class TestLoginPage {
         } else {
             System.out.println("Test Case - Wrong Username and Correct Password: Login successful!");
         }
-
-        driver.quit();
     }
 
-    // Test Case 3:  True Negative
-    public static void testCaseWrongPassword(String username, String password) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/login");
-        driver.manage().window().maximize();
-
+    // Test Case 3: True Negative
+    public void testCaseWrongPassword(String username, String password) throws InterruptedException {
         driver.findElement(By.id("userName")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login")).click();
@@ -90,17 +101,10 @@ public class TestLoginPage {
         } else {
             System.out.println("Test Case - Correct Username and Wrong Password: Login successful!");
         }
-
-        driver.quit();
     }
-    
-    // Test Case 4:  FalseNegative
-    public static void testCaseWrongBoth(String username,String password)  throws InterruptedException
-    {
-    	WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/login");
-        driver.manage().window().maximize();
 
+    // Test Case 4: False Negative
+    public void testCaseWrongBoth(String username, String password) throws InterruptedException {
         driver.findElement(By.id("userName")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login")).click();
@@ -112,9 +116,5 @@ public class TestLoginPage {
         } else {
             System.out.println("Test Case - Wrong Username and Wrong Password: Login successful!");
         }
-
-        driver.quit();
-    	
     }
 }
-
